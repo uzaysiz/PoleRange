b2e536bffc2c64c01de69bee82933c7d61e88a5e
uzayli <uzay@me.me>
2021-08-25 14:57:16 +0300

    initial commit

---
+++ PoleRange_0.2.1/prototypes/item.lua
@@ -0,0 +1,9 @@
+data.raw['electric-pole']['substation'].supply_area_distance = 48;
+data.raw['electric-pole']['small-electric-pole'].supply_area_distance = 48;
+data.raw['electric-pole']['medium-electric-pole'].supply_area_distance = 48;
+data.raw['electric-pole']['big-electric-pole'].supply_area_distance = 48;
+
+data.raw['electric-pole']['substation'].maximum_wire_distance = 48;
+data.raw['electric-pole']['small-electric-pole'].maximum_wire_distance = 48;
+data.raw['electric-pole']['medium-electric-pole'].maximum_wire_distance = 48;
+data.raw['electric-pole']['big-electric-pole'].maximum_wire_distance = 48;
